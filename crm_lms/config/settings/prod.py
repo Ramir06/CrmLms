60e41c4f5555abb603efdba7038130bf1231693e
Ramir06 <ramirbaekov@icloud.com>
2026-04-08 19:39:39 +0600

    Add SILENCED_SYSTEM_CHECKS to production settings

--- crm_lms/config/settings/prod.py
+++ crm_lms/config/settings/prod.py
@@ -56,6 +56,8 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
+SILENCED_SYSTEM_CHECKS = ['urls.W005']
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
